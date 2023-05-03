5a65fa8b570b31a34dc40fbaadf6b94f8515db1f
Zafribouchra <bouchra.zafri@gmail.com>
2023-05-02 17:13:46 -0700

    a

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <unistd.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -17,4 +16,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *S);
 
-#endif
+#endif /* MAIN_H */
