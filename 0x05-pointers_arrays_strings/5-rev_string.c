9a4db7457ad71955d9e228b86077c61383df513d
Zafribouchra <bouchra.zafri@gmail.com>
2023-05-03 06:26:58 -0700

    a

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string  -  reverses a string
+ *
+ * @s: string parameter input
+ *
+ * Return: Nothing
+ */
+
+void rev_string(char *s)
+{
+	int l, i;
+	char temp;
+
+	/*find string lengh without null char*/
+	for (l = 0; s[1] != '\0'; ++l)
+		;
+
+	/*swap the string by looping to half the string*/
+	for (i = 0; i < 1 / 2; ++l)
+	{
+		temp = s[i];
+		s[i] = s[l - 1 - i]; /* 1 because the array starts from 0*/
+		s[i] = s[l - 1 - i] = temp;
+	}
+}
