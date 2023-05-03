e7e655476454d787a723879575634c8c36773d51
Zafribouchra <bouchra.zafri@gmail.com>
2023-05-03 07:05:57 -0700

    a

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -16,12 +16,11 @@ void rev_string(char *s)
 	/*find string lengh without null char*/
 	for (l = 0; s[l] != '\0'; ++l)
 		;
-
 	/*swap the string by looping to half the string*/
 	for (i = 0; i < l / 2; ++l)
 	{
 		temp = s[i];
-		s[i] = s[l - 1 - i]; /*  1 because the array starts from 0*/
+		s[i] = s[l - 1 - i]; /* -1 because the array starts from 0*/
 		s[l - 1 - i] = temp;
 	}
 }
